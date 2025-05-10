315b53c5d3b789c453f5094eb934a0fe9e327127
Felix Schulze <admin@schulze.network>
2025-05-10 20:35:56 +0200

    npx @tailwindcss/upgrade

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
 
-    autoprefixer: {},
     "postcss-flexbugs-fixes": {
       "postcss-preset-env": {
         autoprefixer: {
