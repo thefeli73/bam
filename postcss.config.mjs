10bc3bd3478a3dd840647a35d80e875beb4da886
Felix Schulze <admin@schulze.network>
2025-11-26 12:42:43 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,27 +2,6 @@
 const config = {
   plugins: {
     '@tailwindcss/postcss': {},
-
-    "postcss-flexbugs-fixes": {
-      "postcss-preset-env": {
-        autoprefixer: {
-          flexbox: "no-2009",
-        },
-        stage: 3,
-        features: {
-          "custom-properties": false,
-        },
-      },
-
-      "@fullhuman/postcss-purgecss": {
-        content: [
-          "./pages/**/*.{js,jsx,ts,tsx}",
-          "./components/**/*.{js,jsx,ts,tsx}",
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body"],
-      },
-    },
     cssnano: {},
   },
 };
